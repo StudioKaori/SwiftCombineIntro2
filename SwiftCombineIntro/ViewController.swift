992000dcd8358a4d7d740d58ddd4c124091c4f9e
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-11-11 16:03:33 +0100

    Add button to the cell

--- SwiftCombineIntro/ViewController.swift
+++ SwiftCombineIntro/ViewController.swift
@@ -9,6 +9,31 @@ import UIKit
 
 class MyCustomTableCell: UITableViewCell {
   
+  private let button: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = .systemPink
+    button.setTitle("Button", for: .normal)
+    button.setTitleColor(.white, for: .normal)
+    return button
+  }()
+  
+  // want to override the initialiser to add it as subview
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    contentView.addSubview(button)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    button.frame = CGRect(x: 10, y: 3, width: contentView.frame.size.width-20, height: contentView.frame.size.height-6)
+  }
+  
+  
 }
 
 class ViewController: UIViewController, UITableViewDataSource {
@@ -58,7 +83,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCustomTableCell else {
       fatalError()
     }
-    cell.textLabel?.text = models[indexPath.row]
+    //cell.textLabel?.text = models[indexPath.row]
     return cell
   }
 
