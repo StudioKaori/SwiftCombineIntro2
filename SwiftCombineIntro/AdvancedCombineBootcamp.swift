7fa2f65db30e53c676b151e8e90bff744e6b2bfa
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-11-14 15:56:20 +0100

    Add error handling

--- SwiftCombineIntro/AdvancedCombineBootcamp.swift
+++ SwiftCombineIntro/AdvancedCombineBootcamp.swift
@@ -20,7 +20,6 @@ class AdvancedCombineDataService {
     let items: [Int] = Array(0..<11)
     
     for x in items.indices {
-      // publishers will stay alive until we cancel them!
       DispatchQueue.main.asyncAfter(deadline: .now() + Double(x)) {
         self.passThroughPublisher.send(items[x])
       }
@@ -31,7 +30,8 @@ class AdvancedCombineDataService {
 class AdvancedCombineBootcampViewModel: ObservableObject {
   
   @Published var data: [String] = []
-
+  @Published var error: String = ""
+  
   let dataService = AdvancedCombineDataService()
   
   var cancellables = Set<AnyCancellable>()
@@ -43,6 +43,27 @@ class AdvancedCombineBootcampViewModel: ObservableObject {
   private func addSubscribers() {
 
     dataService.passThroughPublisher
+    // Sequence operation
+    // .first() -> only first publisher is coming through to the pipeline
+    //  .first()
+    
+    // You can set a condition, ex. the first Int greater than 4
+    //  .first(where: { $0 > 4 })
+    
+    // If you want to handle the error, use tryFirst
+      .tryFirst(where: { int in
+        if int == 3 {
+          // If throw the error here, you will receive the error in the completion
+          // Look at the switch case .failure
+          throw URLError(.badServerResponse)
+        }
+        return int > 4
+        
+        // If the successful case comes first, the error wouldn't be thrown.
+        // This case, successful case is greater than 1
+        //return int > 1
+      })
+    
     // convert Int to String
       .map({ String($0) })
       .sink(receiveCompletion: { completion in
@@ -50,7 +71,8 @@ class AdvancedCombineBootcampViewModel: ObservableObject {
         case .finished:
           print("finished")
         case .failure(let error):
-          print("Error: \(error)")
+          self.error = "Error: \(error.localizedDescription)"
+          print("Error: \(error.localizedDescription)")
         }
       }, receiveValue: { [weak self] returnedValue in
         self?.data.append(returnedValue)
@@ -71,6 +93,10 @@ struct AdvancedCombineBootcamp: View {
             Text($0)
               .font(.largeTitle)
               .fontWeight(.black)
+          }
+          
+          if !vm.error.isEmpty {
+            Text(vm.error)
           }
         }
       }
